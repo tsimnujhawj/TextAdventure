0b3093349a63cffa17f731a3ff389d6c93f0a957
tsimnujhawj <tsimnuj.hawj@gmail.com>
2019-02-23 18:44:20 -0600

    created first state

---
+++ Text101/Assets/AdventureGame.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AdventureGame : MonoBehaviour
+{
+    string storyText = ("There we are. This present moment is perfect simply due to the fact you're experiencing it. Just let go - and fall like a little waterfall. Nothing's gonna make your husband or wife madder than coming home and having a snow-covered dinner. It's beautiful - and we haven't even done anything to it yet. We're trying to teach you a technique here and how to use it.");
+    [SerializeField] Text textComponent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        textComponent.text = storyText;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
