47b1682b2c1d9ff436d1ee872ced68b56f1e678a
tsimnujhawj <tsimnuj.hawj@gmail.com>
2019-02-23 20:24:32 -0600

    added room 1 and 2

--- Text101/Assets/State.cs
+++ Text101/Assets/State.cs
@@ -6,10 +6,16 @@ using UnityEngine;
 public class State : ScriptableObject
 {
     [TextArea(10, 14)] [SerializeField] string storyText;
+    [SerializeField] State[] nextStates;
 
     public string GetStateStory()
     {
         return storyText;
+    }
+
+    public State[] GetNextState()
+    {
+        return nextStates;
     }
 
 }
